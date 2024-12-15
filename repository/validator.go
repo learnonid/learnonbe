6f01e53516346b83ba59969b3e99dec064b1cc94
GilangAndhika <gryzlegrizz@gmail.com>
2024-12-15 17:35:10 +0700

    Repos: Add validation functions for phone numbers and emails

---
+++ repository/validator.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"errors"
+	"regexp"
+)
+
+// Validasi nomor telepon
+func ValidatePhoneNumber(phoneNumber string) error {
+	regex := `^(\+62|62|0)8[1-9][0-9]{6,9}$`
+	match, _ := regexp.MatchString(regex, phoneNumber)
+	if !match {
+		return errors.New("Nomor telepon tidak valid")
+	}
+	return nil
+}
+
+// Ubah jika user menginput nomor telepon dengan 08 menjadi +628
+func ChangePhoneNumber(phoneNumber string) string {
+	regex := `^08[1-9][0-9]{6,9}$`
+	match, _ := regexp.MatchString(regex, phoneNumber)
+	if match {
+		phoneNumber = "+62" + phoneNumber[1:]
+	}
+	return phoneNumber
+}
+
+// Validasi email
+func ValidateEmail(email string) error {
+	regex := `^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`
+	match, _ := regexp.MatchString(regex, email)
+	if !match {
+		return errors.New("Email tidak valid")
+	}
+	return nil
+}
