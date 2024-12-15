a48f0cc6c880c72fb0d68b4524b463667495bb5f
GilangAndhika <gryzlegrizz@gmail.com>
2024-12-15 17:31:37 +0700

    Add initial project structure with dependencies and main application setup

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"learnonbe/config"
+	"learnonbe/routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	// Create a new Fiber app
+	app := fiber.New()
+
+	// Connect to database
+	db := config.GetDB()
+
+	// Middleware
+	app.Use(cors.New(cors.Config{
+		AllowHeaders: "*",
+		AllowOrigins: "*",
+		AllowMethods: "*",
+	}))
+
+	app.Use(logger.New(logger.Config{
+		Format: "${status} - ${method} ${path}\n",
+	}))
+
+	// Save the database connection in the app
+	app.Use(func(c *fiber.Ctx) error {
+		c.Locals("db", db)
+		return c.Next()
+	})
+
+	// Routes
+	routes.SetupRoutes(app)
+
+	// Listen to port 3000
+	app.Listen(":3000")
+}
